e6517cdda54966e4156c03ede27e056ca2a7ab62
strollhy <strollhuyac@gmail.com>
2014-02-20 10:18:28 -0600

    Start dev branch

---
+++ code/window.py
@@ -0,0 +1,14 @@
+class Window_Fixed:
+	def __init__(self):
+		pass
+
+class Window:
+	def __init__(self):
+		pass
+
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
---
+++ code/winnow.py
@@ -0,0 +1,13 @@
+class Winnow_Accumulate:
+	def __init__(self):
+		pass
+
+class Winnow_Balance:
+	def __init__(self):
+		pass
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
