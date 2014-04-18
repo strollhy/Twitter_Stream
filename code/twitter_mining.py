e0155af3baca394ee925ec54eba5273c4418a1bd
strollhy <strollhuyac@gmail.com>
2014-04-18 08:33:21 -0500

    correct measurement, combined winnow

--- code/twitter_mining.py
+++ code/twitter_mining.py
@@ -5,65 +5,73 @@ from twitter_sink import TwitterSink
 
 
 c = 0.97
-fp = 0
-tp = 0
-fn = 0
-tn = 0
-
 f = open("../data/out.csv", "w")
 f.close()
+
+
+class Measure:
+    def __init__(self):
+        self.fp = 0
+        self.tp = 0
+        self.fn = 0
+        self.tn = 0
+
+    def eval(self, tup, label):
+
+        fact = tup["label"]
+
+        # Evalutation
+        self.fp *= c
+        self.tp *= c
+        self.fn *= c
+        self.tn *= c
+
+        if label > fact:
+            self.fp += 1
+
+        elif label < fact:
+            self.fn += 1
+
+        elif label == 1:
+            self.tp += 1
+
+        else:
+            self.tn += 1
+
+        acc = (self.tp + self.tn)/(self.tp + self.fn + self.tn + self.fp + .1)
+        pre = self.tp / (self.tp + self.fp + .1)
+        rec = self.tp / (self.tp + self.fn + .1)
+
+        acc = int(acc * 100) / 100.0
+        pre = int(pre * 100) / 100.0
+        rec = int(rec * 100) / 100.0
+
+        print tup["text"]
+        print tup["words"]
+        # print [winnow.stack[word][0] for word in tup["words"] if word in winnow.stack]
+        print ">>>> Get %s but actually is %s" % (label, fact)
+        print ">>>> Accuracy: %s,\t Precision: %s,\t Recall: %s" % (acc, pre, rec)
+        # print ">>>> List length: %s" % len(winnow.stack)
+        print
+
+        out = "%s,%s,%s\n" % (acc, pre, rec)
+        f = open("../data/out.csv", "a")
+        f.write(out)
+        f.close()
 
 
 def train(tup, winnow):
     winnow.add(tup['words'])
     label = winnow.predict(tup['words'])
     winnow.learn(label, tup)
-
-    # Evalutation
-    global fp, tp, fn, tn
-    fact = tup["label"]
-
-    fp *= c
-    tp *= c
-    fn *= c
-    tn *= c
-
-    if label > fact:
-        fp += 1
-
-    elif label < fact:
-        tn += 1
-
-    elif label == 1:
-        tp += 1
-
-    else:
-        fn += 1
-
-    acc = (tp + fn)/(tp + fn + tn + fp + 1)
-    pre = tp / (tp + fp + 1)
-    rec = tp / (tp + fn + 1)
-
-    acc = int(acc * 100) / 100.0
-    pre = int(pre * 100) / 100.0
-    rec = int(rec * 100) / 100.0
-
-    print tup["text"]
-    print tup["words"]
-    print ">>>> Get %s but actually is %s" % (label, fact)
-    print ">>>> Accuracy: %s,\t Precision: %s,\t Recall: %s" % (acc, pre, rec)
-    print ">>>> List length: %s" % len(winnow.stack)
-    print
-
-    out = "%s,%s,%s\n" % (acc, pre, rec)
-    f = open("../data/out.csv", "a")
-    f.write(out)
-    f.close()
+    return label
 
 
 if __name__ == '__main__':
     sink = TwitterSink(["#tech", "#life", "phone"])
-    winnow = BalancedWinnow(1000)
+
+    winnow = [BalancedWinnow(), AccumuWinnow()]
+    measure = Measure()
 
     s1, s2, s000 = None, None, None
 
@@ -71,8 +79,8 @@ if __name__ == '__main__':
         # query each stream
         s_1, s_2, s_000 = sink.stream.cache
 
-        if s_000 and winnow.predict(s_000["words"]):
-            print "Tech tweets found:", s_000["text"]
+        # if s_000 and winnow.predict(s_000["words"]):
+        #     print "Tech tweets found:", s_000["text"]
 
         # if there is update
         if s_1 == s1 or s_2 == s2:
@@ -80,6 +88,17 @@ if __name__ == '__main__':
             continue
 
         s1, s2 = s_1, s_2
-        train(s1, winnow)
-        train(s2, winnow)
+        labels = [train(s1, winnow[0]), train(s1, winnow[1]), s1["label"]]
+        print "=========", labels
+        label = labels[0] and labels[1]
+        # label = train(s1, winnow[0])
+        measure.eval(s1, label)
+
+        labels = [train(s2, winnow[0]), train(s2, winnow[1]), s2["label"]]
+        print "=========", labels
+        label = labels[0] and labels[1]
+
+        # label = train(s1, winnow[0])
+        measure.eval(s2, label)
+
 
--- code/winnow.py
+++ code/winnow.py
@@ -2,13 +2,15 @@
 
 
 """
-learning_rate = 2.0
-
 
 class BalancedWinnow:
-    def __init__(self, size):
-        self.window_size = size
+    """ Balanced Winnow
+
+    Update weight by multiply
+    """
+    def __init__(self):
         self.stack = {}
+        self.rate = 2.0
 
     def add(self, words):
         for word in words:
@@ -19,7 +21,8 @@ class BalancedWinnow:
                 self.stack[word][1] += 1
 
     def predict(self, words):
-        s = sum([self.stack[word][0] for word in words if word in self.stack])
+        w = [self.stack[word][0] for word in words if word in self.stack]
+        s = sum(w)
         if s > len(words)/2:
             return 1
         else:
@@ -37,12 +40,64 @@ class BalancedWinnow:
                 if word not in self.stack:
                     continue
 
-                self.stack[word][0] /= learning_rate
+                self.stack[word][0] /= self.rate
 
         # if true-negative, raise up the weight
         else:
             for word in keywords['words']:
-                self.stack[word][0] *= learning_rate
+                self.stack[word][0] *= self.rate
+
+        # decay window 0.95^100 = 0.003
+        for w in self.stack.keys():
+            self.stack[w][1] *= 0.95
+            if self.stack[w][1] < 0.001:
+                print "=========== Remove word:", w
+                del self.stack[w]
+
+
+class AccumuWinnow():
+    """ Accumulated Winnow
+
+    Update weight by increasing
+    """
+    def __init__(self):
+        self.stack = {}
+        self.rate = 0.01
+
+    def add(self, words):
+        for word in words:
+            if word not in self.stack:
+                # set initial weights and count
+                self.stack[word] = [0, 1]
+            else:
+                self.stack[word][1] += 1
+
+    def predict(self, words):
+        w = [self.stack[word][0] for word in words if word in self.stack]
+        s = sum(w)
+        if s > 0:
+            return 1
+        else:
+            return 0
+
+    def learn(self, label, keywords):
+        fact = keywords['label']
+
+        if label == fact:
+            return
+
+        # if false-positive, cut down the weight
+        if label > fact:
+            for word in keywords['words']:
+                if word not in self.stack:
+                    continue
+
+                self.stack[word][0] -= self.rate
+
+        # if true-negative, raise up the weight
+        else:
+            for word in keywords['words']:
+                self.stack[word][0] += self.rate
 
         # decay window 0.95^100 = 0.003
         for w in self.stack.keys():
